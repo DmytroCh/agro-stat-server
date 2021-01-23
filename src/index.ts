9ebfe1285a7f15d579fca63a5d89c15274f2a621
DmytroCh <dimabest92@gmail.com>
2021-01-23 21:08:17 +0100

    fix cors issue

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 import express from "express";
+import cors from 'cors';
 import * as cron from "node-cron"
 import { pricesController } from "./Controllers/apiController";
 import { updateData } from "./Controllers/cronController"
 
-
 const app = express();
 const port = 8080; // default port to listen
 
+app.use(cors());
 // define a route handler for the default home page
 app.get("/prices", async(req: express.Request, res: express.Response): Promise<void> => {
     await pricesController(req, res);
